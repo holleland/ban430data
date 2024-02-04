65d51fe3edac55a4747c22d3ccb3c7eb85c16b00
holleland <sondre@skalvi.se>
2024-02-04 23:16:57 +0100

    initial

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Daily temperature in Bergen (Florida)
+#'
+#' Data downloaded from seklima.met.no.
+#'
+#' @format ## `bergentemp`
+#' A tsibble with 8,767 rows and 2 columns:
+#' \describe{
+#'   \item{date}{date}
+#'   \item{meanTemp}{Average temperatuer in celsius}
+#' }
+#' @source <https://seklima.met.no>
+"bergentemp"
